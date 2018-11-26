8c4aae0a4168c0c95150b27e797483d96fd76af7
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-26 11:53:56 -0500

    Updated from Brython Server: 11/26/2018 11:53:55 AM

--- santaproj.py
+++ santaproj.py
@@ -11,6 +11,14 @@ class Santa(App):
     def __init__(self):
         super().__init__()
         
+        #background
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = ImageAsset("images/83581c872f38421.jpg")
+        bg = Sprite(bg_asset, (0,0))
+        bg.scale=1.4
+        
+        #sleigh
         sleigh_asset=ImageAsset("images/santa_sleigh_PNG72.png")
         sleigh= Sprite(sleigh_asset, (350, 100))
         sleigh.scale=0.3
