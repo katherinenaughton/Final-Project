35ea49abb7ceec5204a2dd2c5248252aeb183e6e
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-26 11:43:05 -0500

    Updated from Brython Server: 11/26/2018 11:43:04 AM

--- santaproj.py
+++ santaproj.py
@@ -3,3 +3,17 @@ Katie Naughton
 Programming Final Proj
 Sources:
 '''
+
+from ggame import App, SoundAsset, Sound, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, TextAsset
+
+class Santa(App):
+    
+    def __init__(self):
+        super().__init__()
+        
+        sleigh_asset=ImageAsset("images/santa_sleigh_PNG72.png")
+        sleigh= Sprite(sleigh_asset, (300, 200))
+
+myapp = Santa()
+
+myapp.run()
