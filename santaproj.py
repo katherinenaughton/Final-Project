598de49d6e6186b3da0b3780050c7fcbcd45a852
katherinenaughton <42843645+katherinenaughton@users.noreply.github.com>
2018-11-27 12:48:22 -0500

    Updated from Brython Server: 11/27/2018 12:48:21 PM

--- santaproj.py
+++ santaproj.py
@@ -6,7 +6,18 @@ Sources:
 
 from ggame import App, SoundAsset, Sound, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, TextAsset
 
-class Santa(App):
+class House1(Sprite):
+    
+    h1_asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png",
+    Frame(227,0,65,125), 4, 'vertical')
+   
+   
+    
+    def __init__(self, position):
+        super().__init__(House1.h1_asset, position)
+        
+class SantaGame(App):
+    
     
     def __init__(self):
         super().__init__()
@@ -24,12 +35,13 @@ class Santa(App):
         sleigh.scale=0.3
         
         #JingleBells
-        jingleasset = SoundAsset("sounds/JSanta Claus Is Coming To Town.mp3")
-        self.jingle=Sound(Santa.jingleasset)
-        self.jingle.volume=5
+        jingle_asset = SoundAsset("sounds/Santa Claus Is Coming To Town.mp3")
+        jingle=Sound(jingle_asset)
+        jingle.volume=8
+        jingle.play()
 
         
 
-myapp = Santa()
+myapp = SantaGame()
 
 myapp.run()
